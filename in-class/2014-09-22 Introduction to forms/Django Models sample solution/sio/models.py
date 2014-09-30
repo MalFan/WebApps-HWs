ea3dc9d4c9ec83d9607b31098fba10cde532bf4d
Desai Fan <Fantasia@desaitekiair.home>
2014-09-30 10:45:35 -0400

    Inclass updated. Telephone number and zip code can be entered now.

--- in-class/2014-09-22 Introduction to forms/Django Models sample solution/sio/models.py
+++ in-class/2014-09-22 Introduction to forms/Django Models sample solution/sio/models.py
@@ -4,11 +4,11 @@ class Student(models.Model):
     andrew_id = models.CharField(max_length=20, primary_key=True)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    tel_number = models.CharField(max_length=13)
-    zip_code = models.CharField(max_length=6)
+    # tel_number = models.CharField(max_length=13)
+    # zip_code = models.CharField(max_length=6)
     def __unicode__(self):
-        return "%s %s (%s), %s, %s" % (self.first_name, self.last_name, 
-                               self.andrew_id, self.tel_number, self.zip_code)
+        return "%s %s (%s)" % (self.first_name, self.last_name, 
+                               self.andrew_id)
 
 class Course(models.Model):
     course_number = models.CharField(max_length=20, primary_key=True)
