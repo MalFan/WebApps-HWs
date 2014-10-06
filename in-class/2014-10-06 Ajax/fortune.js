2699c1b68bae37716d7d9594576e562f35956e52
Desai Fan <Fantasia@desaitekiair.wv.cc.cmu.edu>
2014-10-06 14:08:49 -0400

    Inclass done.

---
+++ in-class/2014-10-06 Ajax/fortune.js
@@ -0,0 +1,32 @@
+var req;
+
+// Sends a new request to update the to-do list
+function sendRequest() {
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+    } else {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    req.onreadystatechange = handleResponse;
+    req.open("GET", "http://garrod.isri.cmu.edu/webapps/fortune", true);
+    req.send(); 
+}
+
+// This function is called for each request readystatechange,
+// and it will eventually parse the XML response for the request
+function handleResponse() {
+    if (req.readyState != 4 || req.status != 200) {
+        return;
+    }
+
+    // Removes the old to-do list items
+    var p = document.getElementById("content");
+
+    var fortune = JSON.parse(req.responseText);
+
+    var fortune_text = fortune["fortune"];
+
+    p.innerHTML = fortune_text;
+
+}
+
