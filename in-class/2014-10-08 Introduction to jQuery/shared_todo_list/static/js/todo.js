4d03b1c9d38c5bceddc04d52da936fd30765522b
Desai Fan <Fantasia@desaitekiair.home>
2014-10-08 13:31:40 -0400

    Inclass track 1 done.

--- in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
+++ in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
@@ -1,4 +1,10 @@
 // Insert code here to run when the DOM is ready
 $(document).ready( function() {
+	$( ".delete-btn" ).click(function( event ) {
+ 		
+		var id = $( this ).attr( "data-item-id" );
+		$.get( "/shared-todo-list/delete-item/" + id );
+		$( this ).parent().remove();
 
+	});
 });
--- in-class/2014-10-08 Introduction to jQuery/webapps/urls.py
+++ in-class/2014-10-08 Introduction to jQuery/webapps/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^shared-todo-list/', include('shared_todo_list.urls')),
-    url(r'^$', 'shared_todo_list.views.home'),
 )
